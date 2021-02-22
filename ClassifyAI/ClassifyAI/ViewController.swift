a7bfdb589548b6e9e325ec28480b8d5cf07bb0bd
nissy7ok <nishimura.n.ku@gmail.com>
2021-02-22 10:10:40 +0900

    create a swift-app with xcode

---
+++ ClassifyAI/ClassifyAI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClassifyAI
+//
+//  Created by 西村　直恭 on 2021/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
