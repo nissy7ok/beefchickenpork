d48f80e063637d7abd1cd043fa9550017917597b
nissy7ok <nishimura.n.ku@gmail.com>
2021-02-22 11:47:20 +0900

    add function to take photo

--- ClassifyAI/ClassifyAI/ViewController.swift
+++ ClassifyAI/ClassifyAI/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageDisplay: UIImageView!
+    @IBOutlet weak var predictionDisplay: UITextView!
+    var imagePicker: UIImagePickerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
     }
 
-
+    @IBAction func takePhoto(_ sender: Any) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageDisplay.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
 }
 
