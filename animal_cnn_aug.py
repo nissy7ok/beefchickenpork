209384527c8afd3dc2fc16128e718fdb31ffa575
nissy7ok <nishimura.n.ku@gmail.com>
2021-02-25 22:44:44 +0900

    acc 0.85

--- animal_cnn_aug.py
+++ animal_cnn_aug.py
@@ -50,7 +50,7 @@ def model_train(X, y):
     model.add(Flatten())
     model.add(Dense(512))
     model.add(Activation('relu'))
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.25))
     model.add(Dense(3))
     model.add(Activation('softmax'))
 
