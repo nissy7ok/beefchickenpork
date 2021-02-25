10580f0e48e22fede8fef9aa8d360acb4d4bd0ed
nissy7ok <nishimura.n.ku@gmail.com>
2021-02-25 21:04:51 +0900

    reduce layer

--- animal_cnn_aug.py
+++ animal_cnn_aug.py
@@ -44,8 +44,6 @@ def model_train(X, y):
     
     model.add(Conv2D(64, (3,3), padding='same'))
     model.add(Activation('relu'))
-    model.add(Conv2D(64, (3,3)))
-    model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2,2)))
     model.add(Dropout(0.25))
     
