191f530966f466883fd1b8671ce782641cf35eda
nissy7ok <nishimura.n.ku@gmail.com>
2021-02-26 11:56:32 +0900

    add layer in FC layer
    * add dense(64)
    acc 0.88

--- animal_cnn_aug.py
+++ animal_cnn_aug.py
@@ -37,6 +37,7 @@ def model_train(X, y):
     model = Sequential()
     model.add(Conv2D(32, (3,3), padding='same', input_shape=X.shape[1:]))
     model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
     model.add(Conv2D(32, (3,3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2,2)))
@@ -49,6 +50,8 @@ def model_train(X, y):
     
     model.add(Flatten())
     model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dense(64))
     model.add(Activation('relu'))
     model.add(Dropout(0.25))
     model.add(Dense(3))
